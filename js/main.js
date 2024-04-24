0efa2b3572c0216a2d727180b2b22624d4a7261a
asalkhaef <asal.khaef@gmail.com>
2024-04-24 21:09:42 +0430

    add js to register

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 const loginModal = document.getElementById("modal-login")
+const registerModal = document.getElementById("modal-register")
 const overlay = document.getElementById("modal-overlay")
+const loginBtn = document.getElementById("login-button-mainpage")
+const modalCards = document.getElementsByClassName("modal-card")
 
-loginModal.style.display = "block"
-overlay.style.display = "flex"
+const modalCloseBtn = document.getElementsByClassName("modal-close-btn")[0]
+
+const registerBtn = document.getElementById("register-btn")
+
+console.log(modalCloseBtn)
+
+function showLoginModal(){
+    loginModal.style.display = "block"
+    overlay.style.display = "flex"
+}
+
+loginBtn.addEventListener("click", showLoginModal)
+
+function closeModals(){
+    for (let item of modalCards) {
+        item.style.display = "none"
+    }
+    overlay.style.display = "none"
+}
+
+modalCloseBtn.addEventListener("click", closeModals)
+
+function showRegisterModal(){
+    registerModal.style.display = "block"
+    loginModal.style.display = "none"
+}
+
+registerBtn.addEventListener("click", showRegisterModal)
+
+
