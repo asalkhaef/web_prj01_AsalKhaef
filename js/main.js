24626739abf5251eb69cb98e6c9f8e1271f2f187
asalkhaef <asal.khaef@gmail.com>
2024-04-24 20:37:52 +0430

    Show login modal with js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const loginModal = document.getElementById("modal-login")
+const overlay = document.getElementById("modal-overlay")
+
+loginModal.style.display = "block"
+overlay.style.display = "flex"
